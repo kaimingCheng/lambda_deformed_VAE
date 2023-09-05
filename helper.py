aa632b0fff0f68a0b4109a2328b00ad4761a1e78
kaimingCheng <71715273+kaimingCheng@users.noreply.github.com>
2023-09-05 18:27:32 -0400

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,96 +1,14 @@
 import math
-import os
-import pickle
 
 import matplotlib.gridspec as gridspec
 import matplotlib.pyplot as plt
-import numpy as np
 import torch
 import torch.optim as optim
-import torchvision.datasets as dset
-import torchvision.transforms as T
-from scipy.ndimage.filters import gaussian_filter1d
 from torch import nn
-from torch.utils.data import DataLoader, sampler
 
 from vae import loss_function, loss_function_alpha, iwae, iwae_np, vr_max, loss_function_alpha_np
 
 
-def preprocess(img, size=224):
-    transform = T.Compose(
-        [
-            T.Resize(size),
-            T.ToTensor(),
-            T.Normalize(mean=SQUEEZENET_MEAN.tolist(), std=SQUEEZENET_STD.tolist()),
-            T.Lambda(lambda x: x[None]),
-        ]
-    )
-    return transform(img)
-
-
-def deprocess(img, should_rescale=True):
-    # should_rescale true for style transfer
-    transform = T.Compose(
-        [
-            T.Lambda(lambda x: x[0]),
-            T.Normalize(mean=[0, 0, 0], std=(1.0 / SQUEEZENET_STD).tolist()),
-            T.Normalize(mean=(-SQUEEZENET_MEAN).tolist(), std=[1, 1, 1]),
-            T.Lambda(rescale) if should_rescale else T.Lambda(lambda x: x),
-            T.ToPILImage(),
-        ]
-    )
-    return transform(img)
-
-
-def rescale(x):
-    low, high = x.min(), x.max()
-    x_rescaled = (x - low) / (high - low)
-    return x_rescaled
-
-
-def blur_image(X, sigma=1):
-    X_np = X.cpu().clone().numpy()
-    X_np = gaussian_filter1d(X_np, sigma, axis=2)
-    X_np = gaussian_filter1d(X_np, sigma, axis=3)
-    X.copy_(torch.Tensor(X_np).type_as(X))
-    return X
-
-
-# Older versions of scipy.misc.imresize yield different results
-# from newer versions, so we check to make sure scipy is up to date.
-def check_scipy():
-    import scipy
-
-    vnum = int(scipy.__version__.split(".")[1])
-    major_vnum = int(scipy.__version__.split(".")[0])
-
-    assert (
-        vnum >= 16 or major_vnum >= 1
-    ), "You must install SciPy >= 0.16.0 to complete this notebook."
-
-
-def jitter(X, ox, oy):
-    """
-    Helper function to randomly jitter an image.
-
-    Inputs
-    - X: PyTorch Tensor of shape (N, C, H, W)
-    - ox, oy: Integers giving number of pixels to jitter along W and H axes
-
-    Returns: A new PyTorch Tensor of shape (N, C, H, W)
-    """
-    if ox != 0:
-        left = X[:, :, :, :-ox]
-        right = X[:, :, :, -ox:]
-        X = torch.cat([right, left], dim=3)
-    if oy != 0:
-        top = X[:, :, :-oy]
-        bottom = X[:, :, -oy:]
-        X = torch.cat([bottom, top], dim=2)
-    return X
-
-
-### Helper Functions for VAE and GAN
 def show_images(images):
     images = torch.reshape(
         images, [images.shape[0], -1]
@@ -110,19 +28,6 @@ def show_images(images):
         ax.set_aspect("equal")
         plt.imshow(img.reshape([sqrtimg, sqrtimg]))
     return
-
-
-def count_params(model):
-    """Count the number of parameters in the model"""
-    param_count = sum([p.numel() for p in model.parameters()])
-    return param_count
-
-
-def initialize_weights(m):
-    """Initializes the weights of a torch.nn model using xavier initialization"""
-    if isinstance(m, nn.Linear) or isinstance(m, nn.ConvTranspose2d):
-        nn.init.xavier_uniform_(m.weight.data)
-
 
 def one_hot(labels, class_size):
     """
@@ -151,8 +56,7 @@ def train_vae(epoch, model, train_loader, alpha=0.9, cond=False):
     - epoch: Current epoch number
     - model: VAE model object
     - train_loader: PyTorch Dataloader object that contains our training data
-    - cond: Boolean value representing whether we're training a VAE or
-    Conditional VAE
+    - cond: Boolean value representing whether we're training a VAE
     """
     model.train()
     train_loss = 0
